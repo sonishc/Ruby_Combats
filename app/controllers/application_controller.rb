bc98833045259b4c09e229026f1b3088ae44b6a2
Stepan Kovalyshyn <stepan.kovalyshyn@gmail.com>
2017-10-02 14:57:29 +0300

    Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-# ApplicationController
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 class User < ApplicationRecord
-  belongs_to :role
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  belongs_to :role, optional: true
   has_many :inventories
 
-  has_secure_password
+  # has_secure_password
 
   before_save :convert_email_to_downcase
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }
-  validates :name, presence: true,  uniqueness: true, length: { maximum: 99 }
+  # validates :name, presence: true,  uniqueness: true, length: { maximum: 99 }
   validates :password, length: { minimum: 5 }
 
   private
