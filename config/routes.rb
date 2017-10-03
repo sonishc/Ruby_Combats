eeb6120979391798931531df4cceb045ab629e50
VR333 <v.r.3.3.3.lawyer@ukr.net>
2017-10-03 22:54:27 +0300

    Add parameter :name to device, fix bug with react component, resolve 'protect from forgery..' bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'persons/profile'
   devise_for :users
-  root to: 'pages#index'
   get 'persons/profile', as: 'user_root'
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
 end
