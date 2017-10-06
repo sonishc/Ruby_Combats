bf0c3e8d3977172c3b8ddb796b285f000ca3e3c9
Stepan Kovalyshyn <stepan.kovalyshyn@gmail.com>
2017-10-06 06:29:14 +0300

    Preparing to new branch

--- app/assets/javascripts/components/loginForm.jsx
+++ app/assets/javascripts/components/loginForm.jsx
@@ -1,4 +1,31 @@
 class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    axios.post('/users/sign_in', {user: {
+      email: this.state.email,
+      password: this.state.password,
+      }
+    })
+  .then(function (response) {
+    console.log(response);
+    // if (response.status == 200) {
+    //  console.log(location);
+    //  window.location.assign('http://localhost:3000/persons/profile');
+  // }
+  })
+  .catch(function (error) {
+    console.log(error);
+    });
+  }
+
   render(){
     return(
         <div className="wrapper">
@@ -6,7 +33,7 @@ class LoginForm extends React.Component {
             <link href='https://fonts.googleapis.com/css?family=Open+Sans:700,600' rel='stylesheet' type='text/css'/>
             <form className="new_user"
                   id="new_user"
-                  action="/users/sign_in"
+                  // action="/users/sign_in"
                   acceptCharset="UTF-8"
                   method="post">
               <fieldset>
@@ -15,19 +42,23 @@ class LoginForm extends React.Component {
                     <input className="email"
                            id="user_email"
                            type="email"
+                           value={this.state.email}
+                           onChange={e => this.setState({email: e.target.value})}
                            name="user[email]"
                            placeholder="Enter email" 
                            required />
                     <input className="email"
                            id="user_password"
                            type="password"
+                           value={this.state.password}
+                           onChange={e => this.setState({password: e.target.value})}
                            name="user[password]"
                            placeholder="Enter password" 
                            required />
-                    <input type="submit"
-                           name="commit"
-                           value="Log in"
-                           className="btn"/>
+               <button
+                     onClick={this.handleSubmit}
+                     className="btn">Submit!
+               </button>
                     <a href="/users/sign_up" className="btn">Sign Up</a>
                 </div>
             </fieldset>
@@ -39,3 +70,5 @@ class LoginForm extends React.Component {
     );
   }
 }
+
+
