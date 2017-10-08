b0d6a18bbb3e3b5c6dab716ecaeb3c83a7c66821
Stepan Kovalyshyn <stepan.kovalyshyn@gmail.com>
2017-10-08 16:03:29 +0300

    Deleting other files

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,5 @@
 class Skill < ApplicationRecord
   belongs_to :user
-  # validates_presence_of :power, :dexterity, :instinct, :stamina, :experience
   validates :power, :dexterity, :instinct,:stamina, :experience,
             numericality: { only_integer: true }
   validates_inclusion_of :power, :dexterity, :instinct, :stamina, in: 1..1_000
