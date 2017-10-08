5765ba31615b8fbae0796453597fb7a0aaf2110c
VR333 <v.r.3.3.3.lawyer@ukr.net>
2017-10-08 14:30:10 +0300

    Add registration with redirect

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  belongs_to :role
+
   has_many :inventories
 
   before_save :convert_email_to_downcase
@@ -12,7 +10,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }
   validates :name, presence: true,  uniqueness: true, length: { maximum: 99 }
-  validates :password, length: { minimum: 5 }
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'persons/profile', to: 'persons#profile'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    post '/users/signup', to: 'registrations#create'
+    root to: 'devise/sessions#new'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+    get '/users/edit', to: 'devise/registrations#edit'
+  end
 end
