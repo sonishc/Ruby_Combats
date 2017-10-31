ca15765708478734176f237a3aae7e960e0afb6b
Stepan Kovalyshyn <stepan.kovalyshyn@gmail.com>
2017-11-01 00:00:07 +0200

    Adding chat with internationalization

---
+++ app/assets/javascripts/components/faye_messages.jsx
@@ -0,0 +1,40 @@
+class FayeMessages extends React.Component {
+
+  generatePrivateChat(event) {
+    let name = event.target.innerHTML;
+    if (!document.getElementById(`#${name}`)) {
+      document.getElementById('message').value = '@'+name+' (private message): ';
+    }
+  };
+
+  generateUsersOnline(user_list) {
+    let users = user_list.map((user, index) => {
+      if(user.name !== this.props.name){
+        return (<p className="users_list" onClick={this.generatePrivateChat.bind(this)} key={index}>{user.name}</p>);
+      }
+    });
+    return users;
+  }
+
+  render() {
+    return (
+      <div>
+        <p className='chat_word'>{ I18n.t('chat.chat_title') }</p>
+        <div className="chat_container" >
+          { messageBox(this.props.name) }
+          <div id="container"></div>
+          <div id="chat_room" className="chat_order" >
+            <div className="user_list">
+              { this.generateUsersOnline(this.props.users) }
+            </div>
+            <div className="message_list" id="list"></div>
+          </div>
+          <form id="new_message_form" method="post" className="chat_input" data-remote="true">
+            <input type="text" id="message" name="message" className="input_location" autoComplete="off"/>
+            <input type="submit" value={ I18n.t('chat.send')} className = "btn btn-danger nav-button button_location"/>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ app/assets/javascripts/services/faye_messages.jsx
@@ -0,0 +1,51 @@
+function messageBox(name) {
+
+  $(function() {
+    let client = new Faye.Client('http://localhost:9292/faye');
+    let time = new Date();
+    let messageToBottom = document.getElementById('list');
+
+    $('#new_message_form').submit(function(){
+      if (matches = $('#message').val().match(/@(\w+) (.+)/)) {
+        client.publish('/messages/private/' + matches[1], {
+          username: name,
+          msg: matches[2],
+          time: time.toLocaleTimeString()
+        });
+      if(matches[1] !== name){
+        let formTime = time.toLocaleTimeString(), 
+            createData = document.createElement('p'),
+            paragraphWithText = formTime +' - '+name+' '+matches[2],
+            addTime = document.createTextNode(paragraphWithText),
+            paragraph = createData.appendChild(addTime);
+        createData.className = "floralwhite_fone";
+        document.getElementsByClassName('message_list')[0].appendChild(createData).appendChild(paragraph);
+        }
+      }
+      else {
+        client.publish('/messages/public', {
+          username: name,
+          msg: $('#message').val(),
+          time: time.toLocaleTimeString()
+        });
+      }
+
+      $('.message_list').on('DOMNodeInserted', 'p', function () {
+        setTimeout(function() {
+          messageToBottom.scrollTop = messageToBottom.scrollHeight;
+        }, 100);
+      });
+
+      $('#message').val('')
+        return false;
+      });
+
+    let public_subscription = client.subscribe('/messages/public', function(data) {
+      $('<p class="dark_fone" ></p>').html(data.time  + " - " + data.username + ": " + data.msg).appendTo('.message_list');
+    }); 
+
+    let private_subscription = client.subscribe('/messages/private/' + name, function(data) {
+      $('<p class="floralwhite_fone" ></p>').html(data.time  + " - " + data.username + ": " + data.msg).appendTo('.message_list');
+    }); 
+  });
+}
