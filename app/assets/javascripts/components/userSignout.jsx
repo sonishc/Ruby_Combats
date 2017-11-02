18e35febaaade70591c63f45c0d8e25964b26328
Andriy Braylyak <andriy.braylyak@gmail.com>
2017-11-03 01:09:33 +0200

    last

--- app/assets/javascripts/components/userSignout.jsx
+++ app/assets/javascripts/components/userSignout.jsx
@@ -2,32 +2,24 @@ class UserSignout extends React.Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = { redirect: false };
-
-    console.log(props.user);
-  }
-
-  initHeaders() {
-    const token = document.querySelector("meta[name=csrf-token]").getAttribute('content');
-    axios.defaults.headers.common['X-CSRF-Token'] = token
-    axios.defaults.headers.common['Accept'] = 'application/json'
   }
 
   handleSubmit(e) {
-      $.ajax({
-         url:'/users/sign_out',
-         method: 'delete',
-       }).done(function(response) {
-        console.log('success');
-         });
-  }
+    $.ajax({
+        url: '/users/sign_out',
+        type: 'DELETE',
+        success: function(result) {
+            console.log('Success!');
+        }
+    });
+}
 
 render(){
    return(
        <div>
          <li>
             <a href="#" onClick={this.handleSubmit}>
-              <span className="glyphicon glyphicon-log-out"></span> Log out
+              <span className="glyphicon glyphicon-log-out"></span> Sign out
             </a>
          </li>
        </div>
