0ec11457f3700e63982522255c801c11a8b649ba
webandriy <andriy.braylyak@gmail.com>
2017-11-02 19:28:56 +0200

    Add a possibility to revise a history of battles

--- app/assets/javascripts/components/fight.jsx
+++ app/assets/javascripts/components/fight.jsx
@@ -1,35 +1,63 @@
+class Fight extends React.Component {
+  constructor(props) {
+    super(props);
+    const user = this.setStats(JSON.parse(props.user));
 
-faye_log(data) {
+    this.state = {
+      logs : []
+    };
+  }  
+
+  getDamage(actor) {
+    let max = 0, armor_bonus = 0;
+    if (actor == 'user') {
+      max = USER_DAMAGE;
+    } else {
+      armor_bonus = Math.floor(this.state.user.armor * ARMOR_MULTIPLIER);
+      max = BOT_DAMAGE;
+    }
+    const random = this.getRandomInt(1, max);
+    const actual = random - armor_bonus;
+    let loginf = (`The ${actor}  defends on:  ${armor_bonus} health points and demaged on: ${actual} health points`);
+    this.setState({logs:[
+        {
+          "name":"actor",
+          "data":actor
+        },
+        {
+          "name":"armor",
+          "data":armor_bonus
+        },
+        {
+          "name":"hit",
+          "data":actual
+        }
+      ]
+    });
+    this.setTo(loginf);
+    this.faye_log(loginf);
+    return actual >= 0 ? actual : 1;
+  }
+
+  faye_log(data) {
     $(function() {
-    var client = new Faye.Client('http://localhost:9292/faye');
-    var time = new Date();
-    var message_to_bottom = document.getElementById('chat_room');
-
-      $(function(){
-    
-        client.publish('/messages/public', {
-          username: name,
-          msg: data,
-          time: time.toLocaleTimeString()
+      let client = new Faye.Client(`http://localhost:9292/faye`);
+      let time = new Date();
+      let message_to_bottom = document.getElementById(`chat_room`);
+        $(function(){
+          client.publish(`/messages/fight`, {
+            username: name,
+            msg: data,
+            time: time.toLocaleTimeString()
         });
-
-        $('.chat_order').on('DOMNodeInserted', 'p', function () {
-          window.setTimeout(function() {
-            message_to_bottom.scrollTop = message_to_bottom.scrollHeight;
-          }, 100);
-        });
-   
-        var public_subscription = client.subscribe('/messages/public', function(data) {
-          $('<p class="dark_fone" ></p>').html(data.time  + " - " + data.msg).appendTo('#chat_room');
-        }); 
       });
     });
   }
 
   setTo(logs){
-    let d = new Date();
-    let n = d.getTime();
-    let time = n.toString();
+    let date = new Date();
+    let time_t = date.getTime();
+    let time = time_t.toString();
         if (typeof(Storage) !== "undefined") {
           localStorage.setItem(time, logs);
         } else {
@@ -38,8 +66,21 @@ faye_log(data) {
    }
 
   setCookie(cname, cvalue, exdays) {
-    var d = new Date();
+    let date = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
+    let expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
   }
+
+  render () {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <LogWindow 
+           logs={this.state.logs}
+          />
+        </div>
+      </div>
+    )
+  }
+}
--- app/assets/javascripts/components/logWindow.jsx
+++ app/assets/javascripts/components/logWindow.jsx
@@ -1,12 +1,10 @@
 class LogWindow extends React.Component {
-
 	generateLogs(values){		
 	    const renObjData = values.map((value,index) => {
    			return  (<p key={index}>{value.name} - {value.data}</p>);
    		});
 		return renObjData;
 	}
-
   render () {
     return (
     	<div className="col-md-3 pull-right log">
